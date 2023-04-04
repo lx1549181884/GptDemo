4562c6faf9ab520b28c6b0d694f75e685c775501
linxing <1549181884@qq.com>
2023-04-04 15:40:15 +0800

    bugly

--- app/src/main/java/com/rick/openaidemo/AppAc.kt
+++ app/src/main/java/com/rick/openaidemo/AppAc.kt
@@ -6,9 +6,11 @@ import com.blankj.utilcode.util.ToastUtils
 import com.rick.jetpackmvvm.base.BaseActivity
 import com.rick.jetpackmvvm.base.BaseViewModel
 import com.rick.openaidemo.databinding.AcAppBinding
+import com.tencent.bugly.crashreport.CrashReport
 
 class AppAc : BaseActivity<AcAppBinding, BaseViewModel>() {
     override fun init(binding: AcAppBinding, vm: BaseViewModel) {
+        CrashReport.initCrashReport(applicationContext, "44952c159f", true);
         supportActionBar?.hide()
         ToastUtils.getDefaultMaker().setBgColor(Color.BLACK).setTextColor(Color.WHITE)
             .setGravity(Gravity.CENTER, 0, 0)
